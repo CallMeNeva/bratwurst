dd25ed54fdec20a676faa40ddfd568854de89cda
altoukhov-max <86380488+altoukhov-max@users.noreply.github.com>
2021-11-19 08:42:20 +0300

    Updated Currency and ExchangeRate (WIP)
    * ExchangeRate base and targets are now of type Currency
    * ExchangeRate value is now of type BigDecimal
    * Both classes now have a short description comment next to their declarations

--- src/main/java/io/github/altoukhovmax/frankfurterdesktop/model/Currency.java
+++ src/main/java/io/github/altoukhovmax/frankfurterdesktop/model/Currency.java
@@ -2,6 +2,11 @@ package io.github.altoukhovmax.frankfurterdesktop.model;
 
 import org.jetbrains.annotations.NotNull;
 
-public record Currency(@NotNull String code,
-                       @NotNull String displayName) {
+/*
+ * Models a currency. Each one is represented by:
+ *     - an ISO-4217 code, e.g. "USD";
+ *     - a display name, e.g. "United Stated Dollar".
+ * None of the values may be null. This class is immutable.
+ */
+public record Currency(@NotNull String code, @NotNull String displayName) {
 }
--- src/main/java/io/github/altoukhovmax/frankfurterdesktop/model/ExchangeRate.java
+++ src/main/java/io/github/altoukhovmax/frankfurterdesktop/model/ExchangeRate.java
@@ -2,10 +2,19 @@ package io.github.altoukhovmax.frankfurterdesktop.model;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record ExchangeRate(@NotNull String baseCurrencyCode,
-                           @NotNull String targetCurrencyCode,
-                           double factor,
+/*
+ * Models an exchange rate. Each one is represented by:
+ *     - a base currency;
+ *     - a target currency;
+ *     - the actual exchange rate value;
+ *     - a publication date.
+ * None of the values may be null. This class is immutable.
+ */
+public record ExchangeRate(@NotNull Currency baseCurrency,
+                           @NotNull Currency targetCurrency,
+                           @NotNull BigDecimal value,
                            @NotNull LocalDate publicationDate) {
 }
