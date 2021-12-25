e56f67ff37af888fdfa50290ab0cacd675990e41
altoukhov-max <86380488+altoukhov-max@users.noreply.github.com>
2021-12-26 02:44:46 +0300

    Removed service interface, updated impl

--- src/main/java/io/github/altoukhovmax/frankfurterdesktop/service/FrankfurterService.java
+++ src/main/java/io/github/altoukhovmax/frankfurterdesktop/service/FrankfurterService.java
@@ -19,90 +19,96 @@ package io.github.altoukhovmax.frankfurterdesktop.service;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.github.altoukhovmax.frankfurterdesktop.model.Currency;
+import io.github.altoukhovmax.frankfurterdesktop.model.CurrencyRegistry;
 import io.github.altoukhovmax.frankfurterdesktop.model.ExchangeRate;
+import io.github.altoukhovmax.frankfurterdesktop.service.request.AbstractDataRequest;
 import io.github.altoukhovmax.frankfurterdesktop.service.request.AbstractExchangeRatesRequest;
-import io.github.altoukhovmax.frankfurterdesktop.service.request.DataRequest;
+import io.github.altoukhovmax.frankfurterdesktop.service.request.CurrenciesRequest;
 import io.github.altoukhovmax.frankfurterdesktop.service.request.HistoricalExchangeRatesRequest;
 import io.github.altoukhovmax.frankfurterdesktop.service.request.LatestExchangeRatesRequest;
 import io.github.altoukhovmax.frankfurterdesktop.service.request.TimeSeriesExchangeRatesRequest;
+import io.github.altoukhovmax.frankfurterdesktop.service.response.CurrenciesDTO;
 import io.github.altoukhovmax.frankfurterdesktop.service.response.SpecificDateExchangeRatesDTO;
 import io.github.altoukhovmax.frankfurterdesktop.service.response.TimeSeriesExchangeRatesDTO;
+import io.github.altoukhovmax.frankfurterdesktop.service.response.mapper.CurrenciesDTOMapper;
 import io.github.altoukhovmax.frankfurterdesktop.service.response.mapper.DTOMapper;
 import io.github.altoukhovmax.frankfurterdesktop.service.response.mapper.DTOMappingException;
 import io.github.altoukhovmax.frankfurterdesktop.service.response.mapper.SpecificDateExchangeRatesDTOMapper;
 import io.github.altoukhovmax.frankfurterdesktop.service.response.mapper.TimeSeriesExchangeRatesDTOMapper;
+import org.apache.hc.client5.http.fluent.Request;
+import org.apache.hc.core5.http.HttpHost;
+import org.apache.hc.core5.util.Timeout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
 import java.util.Objects;
 import java.util.Set;
-import java.util.logging.Logger;
 
-public final class FrankfurterService implements ExchangeRatesService {
+public final class FrankfurterService {
 
-    private static final Logger LOGGER = Logger.getLogger(FrankfurterService.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(FrankfurterService.class);
     private static final ObjectMapper JSON_PARSER = new ObjectMapper()
             .registerModule(new JavaTimeModule())
-            .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-    private static final Duration TIMEOUT_DURATION = Duration.ofSeconds(8);
+    private static final Timeout CONNECTION_TIMEOUT = Timeout.ofSeconds(8);
+    private static final Timeout RESPONSE_TIMEOUT = Timeout.ofSeconds(8);
 
-    private final String resourceIdentifierBase;
-    private final HttpClient client;
+    private HttpHost host;
 
-    private FrankfurterService(String hostname, boolean enableSecureConnection) {
-        this.resourceIdentifierBase = String.format("%s://%s/", enableSecureConnection ? "https" : "http", hostname);
-        this.client = HttpClient.newHttpClient();
+    public FrankfurterService(HttpHost host) {
+        setHost(host);
     }
 
-    public static FrankfurterService withPublicHost() {
-        return new FrankfurterService("api.frankfurter.app", true);
+    public HttpHost getHost() {
+        return host;
     }
 
-    public static FrankfurterService withCustomHost(String hostname, boolean enableSecureConnection) {
-        /* Hostname is validated at request time instead of construction time */
-        return new FrankfurterService(Objects.requireNonNull(hostname), enableSecureConnection);
+    public void setHost(HttpHost host) {
+        this.host = Objects.requireNonNull(host, "Provided HTTP host is null");
     }
 
-    @Override
-    public Set<ExchangeRate> serve(AbstractExchangeRatesRequest request) throws ServiceException {
-        /*
-         * Since the currency dataset is defined statically with no way to update it at runtime, a DTOMappingException
-         * will occur in the (unlikely) event that it is not up-to-date.
-         */
+    /* Registry should be updated at least once prior to calling this */
+    public Set<ExchangeRate> fetchExchangeRates(AbstractExchangeRatesRequest request) throws ServiceException {
         return switch (request) {
             case LatestExchangeRatesRequest ignored ->
                     fetchData(request, SpecificDateExchangeRatesDTO.class, SpecificDateExchangeRatesDTOMapper.INSTANCE);
-            /* TODO: Merge this with case above when (if) it becomes possible */
-            case HistoricalExchangeRatesRequest ignored ->
+            case HistoricalExchangeRatesRequest ignored -> /* TODO: Merge this with case above when (if) it becomes possible */
                     fetchData(request, SpecificDateExchangeRatesDTO.class, SpecificDateExchangeRatesDTOMapper.INSTANCE);
             case TimeSeriesExchangeRatesRequest ignored ->
                     fetchData(request, TimeSeriesExchangeRatesDTO.class, TimeSeriesExchangeRatesDTOMapper.INSTANCE);
+            case default -> throw new IllegalArgumentException("Unknown request type");
             case null -> throw new IllegalArgumentException("Request is null");
         };
     }
 
-    private <D, T> T fetchData(DataRequest dataRequest,
-                               Class<D> dataObjClass,
-                               DTOMapper<D, T> dataObjMapper) throws ServiceException {
+    public void updateCurrencyRegistry() {
+        Set<Currency> currencies = fetchData(CurrenciesRequest.getInstance(), CurrenciesDTO.class, CurrenciesDTOMapper.INSTANCE);
+        CurrencyRegistry.INSTANCE.overwrite(currencies);
+    }
+
+    private <D, T> T fetchData(AbstractDataRequest dataRequest, Class<D> dataClass, DTOMapper<D, T> dataMapper) throws ServiceException {
         try {
-            URI resourceIdentifier = new URI(resourceIdentifierBase + dataRequest.toURIPath());
-            HttpRequest request = HttpRequest.newBuilder(resourceIdentifier)
-                    .GET()
-                    .timeout(TIMEOUT_DURATION)
-                    .build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            D dataObj = JSON_PARSER.readValue(response.body(), dataObjClass);
-            T data = dataObjMapper.map(dataObj);
+            URI resourceIdentifier = dataRequest.toURIWithHost(host);
+            String responseContent = Request.get(resourceIdentifier)
+                    .connectTimeout(CONNECTION_TIMEOUT)
+                    .responseTimeout(RESPONSE_TIMEOUT)
+                    .execute()
+                    .returnContent()
+                    .asString();
+
+            D dataObject = JSON_PARSER.readValue(responseContent, dataClass);
+            T mappedData = dataMapper.map(dataObject);
+
             LOGGER.info("Fetched: " + resourceIdentifier);
-            return data;
-        } catch (URISyntaxException | IOException | InterruptedException | SecurityException | DTOMappingException e) {
-            LOGGER.warning("Failed to fetch data (" + e.getCause() + ')');
+            return mappedData;
+
+        } catch (URISyntaxException | IOException | DTOMappingException e) {
+            LOGGER.error("Failed to fetch resource (cause: " + e.getCause() + ')');
             throw new ServiceException(e);
         }
     }
