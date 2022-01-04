35c930d18c772f724337d165999a74932ce69075
Max Altoukhov <86380488+altoukhov-max@users.noreply.github.com>
2022-01-04 19:12:46 +0300

    GUI entry point cleanup

--- src/main/java/com/altoukhov/frankfurterdesktop/gui/FrankfurterDesktopGUI.java
+++ src/main/java/com/altoukhov/frankfurterdesktop/gui/FrankfurterDesktopGUI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Maxim Altoukhov
+ * Copyright 2021, 2022 Maxim Altoukhov
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,39 +16,21 @@
 
 package com.altoukhov.frankfurterdesktop.gui;
 
-import com.altoukhov.frankfurterdesktop.gui.scene.FrankfurterServiceSceneFactory;
+import com.altoukhov.frankfurterdesktop.gui.notification.Notification;
+import com.altoukhov.frankfurterdesktop.gui.scene.FrankfurterServiceSceneBuilder;
+import com.altoukhov.frankfurterdesktop.gui.util.action.CurrencySyncAction;
 import com.altoukhov.frankfurterdesktop.service.FrankfurterService;
 import javafx.application.Application;
 import javafx.collections.ListChangeListener;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.stage.Window;
-import org.apache.hc.core5.http.HttpHost;
-import org.apache.hc.core5.http.URIScheme;
-import org.controlsfx.dialog.ExceptionDialog;
 
 public final class FrankfurterDesktopGUI extends Application {
 
     private static final String WINDOW_TITLE = "Frankfurter Desktop";
     private static final Image WINDOW_ICON = new Image("/icons/exchange-rate.png");
-
-    private static final String INPUT_ERROR_MESSAGE = "Invalid input. Please make sure you filled out the form correctly.";
-    private static final String SERVICE_ERROR_MESSAGE =
-            "A service error has occurred. Please make sure you have a reliable network connection. If the error persists, please " +
-            "contact the developer or open an issue on GitHub.";
-    private static final String UNKNOWN_EXCEPTION_DIALOG_HEADER = "Oops!";
-    private static final String UNKNOWN_EXCEPTION_DIALOG_BODY =
-            "An unforeseen error has occurred. Please contact the developer or open an issue on GitHub.";
-
-    private final Alert inputErrorAlert;
-    private final Alert serviceErrorAlert;
-
-    public FrankfurterDesktopGUI() {
-        inputErrorAlert = createAlert(Alert.AlertType.ERROR, INPUT_ERROR_MESSAGE);
-        serviceErrorAlert = createAlert(Alert.AlertType.ERROR, SERVICE_ERROR_MESSAGE);
-    }
 
     @Override
     public void init() {
@@ -67,24 +49,20 @@ public final class FrankfurterDesktopGUI extends Application {
 
     @Override
     public void start(Stage stage) {
-        HttpHost host = new HttpHost(URIScheme.HTTPS.getId(), "api.frankfurter.app");
-        FrankfurterService service = new FrankfurterService(host);
-        Scene scene = FrankfurterServiceSceneFactory.create(service);
+        FrankfurterService service = new FrankfurterService();
+
+        CurrencySyncAction initialCurrencySyncAction = new CurrencySyncAction(service, Notification.failedInitialCurrencySync());
+        initialCurrencySyncAction.run();
+
+        Scene scene = new FrankfurterServiceSceneBuilder(service)
+                .aboutNotification(Notification.about())
+                .currencySyncNotification(Notification.successfulCurrencySync())
+                .hostChangeNotification(Notification.hostChange())
+                .serviceExceptionHandler(Notification.serviceError())
+                .badInputExceptionHandler(Notification.badSheetInput())
+                .build();
+
         stage.setScene(scene);
         stage.show();
-    }
-
-    private Alert createAlert(Alert.AlertType alertType, String message) {
-        Alert alert = new Alert(alertType);
-        alert.setHeaderText(null);
-        alert.setContentText(message);
-        return alert;
-    }
-
-    private void createAndShowExceptionDialog(Exception e) {
-        ExceptionDialog dialog = new ExceptionDialog(e);
-        dialog.setHeaderText(UNKNOWN_EXCEPTION_DIALOG_HEADER);
-        dialog.setContentText(UNKNOWN_EXCEPTION_DIALOG_BODY);
-        dialog.showAndWait();
     }
 }
