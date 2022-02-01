97f3fb30c1fb715ee67fcf5c1d51e25b098d0e39
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-01 10:21:19 +0300

    Update FrankfurterService, remove empty test class

--- src/main/java/com/altoukhov/bratwurst/service/FrankfurterService.java
+++ src/main/java/com/altoukhov/bratwurst/service/FrankfurterService.java
@@ -4,10 +4,11 @@
 package com.altoukhov.bratwurst.service;
 
 import com.altoukhov.bratwurst.model.Currency;
+import com.altoukhov.bratwurst.model.CurrencyRepository;
 import com.altoukhov.bratwurst.model.Exchange;
-import com.altoukhov.bratwurst.service.request.AbstractDataRequest;
 import com.altoukhov.bratwurst.service.request.AbstractExchangeDataRequest;
 import com.altoukhov.bratwurst.service.request.CurrencyDataRequest;
+import com.altoukhov.bratwurst.service.request.DataRequest;
 import com.altoukhov.bratwurst.service.request.HistoricalExchangeDataRequest;
 import com.altoukhov.bratwurst.service.request.LatestExchangeDataRequest;
 import com.altoukhov.bratwurst.service.request.TimeSeriesExchangeDataRequest;
@@ -19,6 +20,7 @@ import com.altoukhov.bratwurst.service.response.mapper.DTOMapper;
 import com.altoukhov.bratwurst.service.response.mapper.DTOMappingException;
 import com.altoukhov.bratwurst.service.response.mapper.SpecificDateExchangesDTOMapper;
 import com.altoukhov.bratwurst.service.response.mapper.TimeSeriesExchangesDTOMapper;
+import com.altoukhov.bratwurst.util.Arguments;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -32,57 +34,70 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Objects;
 import java.util.Set;
 
 public final class FrankfurterService {
 
-    public static final HttpHost PUBLIC_HOST = new HttpHost(URIScheme.HTTPS.getId(), "api.frankfurter.app");
+    private static final HttpHost DEFAULT_HOST = new HttpHost(URIScheme.HTTPS.getId(), "api.frankfurter.app");
+    private static final Timeout CONNECTION_TIMEOUT = Timeout.ofSeconds(8);
+    private static final Timeout RESPONSE_TIMEOUT = Timeout.ofSeconds(8);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(FrankfurterService.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FrankfurterService.class);
     private static final ObjectMapper JSON_PARSER = new ObjectMapper()
             .registerModule(new JavaTimeModule())
             .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
-    private static final Timeout CONNECTION_TIMEOUT = Timeout.ofSeconds(8);
-    private static final Timeout RESPONSE_TIMEOUT = Timeout.ofSeconds(8);
-
+    private final CurrencyRepository currencyRepository;
+    private final SpecificDateExchangesDTOMapper specificDateMapper;
+    private final TimeSeriesExchangesDTOMapper timeSeriesMapper;
     private HttpHost host;
 
-    public FrankfurterService(HttpHost host) {
-        setHost(host);
+    public static FrankfurterService withCustomHost(HttpHost host) {
+        Arguments.checkNull(host, "host");
+        return new FrankfurterService(host);
     }
 
-    public FrankfurterService() {
-        host = PUBLIC_HOST;
+    public static FrankfurterService withDefaultHost() {
+        return new FrankfurterService(DEFAULT_HOST);
+    }
+
+    private FrankfurterService(HttpHost host) {
+        currencyRepository = new CurrencyRepository();
+        specificDateMapper = new SpecificDateExchangesDTOMapper(currencyRepository);
+        timeSeriesMapper = new TimeSeriesExchangesDTOMapper(currencyRepository);
+        this.host = host;
     }
 
     public HttpHost getHost() {
         return host;
     }
 
-    public void setHost(HttpHost host) {
-        this.host = Objects.requireNonNull(host, "Provided HTTP host is null");
-        LOGGER.info("Changed host to " + host);
+    public void setCustomHost(HttpHost host) {
+        this.host = Arguments.checkNull(host, "host");
     }
 
-    /* NOTE: Global registry should be updated at least once prior to calling this */
-    public Set<Exchange> serveExchanges(AbstractExchangeDataRequest request) throws ServiceException {
+    public void setDefaultHost() {
+        host = DEFAULT_HOST;
+    }
+
+    public Set<Exchange> fetchExchanges(AbstractExchangeDataRequest request) throws ServiceException {
+        Arguments.checkNull(request, "request"); // Used over a switch null case for the generic exception message
         return switch (request) {
-            /* FIXME: Merge first two cases when (if) it becomes possible */
-            case LatestExchangeDataRequest r -> serve(request, SpecificDateExchangesDTO.class, SpecificDateExchangesDTOMapper.INSTANCE);
-            case HistoricalExchangeDataRequest r -> serve(request, SpecificDateExchangesDTO.class, SpecificDateExchangesDTOMapper.INSTANCE);
-            case TimeSeriesExchangeDataRequest r -> serve(request, TimeSeriesExchangesDTO.class, TimeSeriesExchangesDTOMapper.INSTANCE);
+            // TODO: Merge first two cases when (if) it becomes possible
+            case LatestExchangeDataRequest ignored -> fetch(request, SpecificDateExchangesDTO.class, specificDateMapper);
+            case HistoricalExchangeDataRequest ignored -> fetch(request, SpecificDateExchangesDTO.class, specificDateMapper);
+            case TimeSeriesExchangeDataRequest ignored -> fetch(request, TimeSeriesExchangesDTO.class, timeSeriesMapper);
             case default -> throw new IllegalArgumentException("Unknown request type");
-            case null -> throw new IllegalArgumentException("Provided request is null");
         };
     }
 
-    public Set<Currency> serveCurrencies() throws ServiceException {
-        return serve(CurrencyDataRequest.INSTANCE, CurrenciesDTO.class, CurrenciesDTOMapper.INSTANCE);
+    public void updateCurrencyRepository() throws ServiceException {
+        Set<Currency> currencies = fetch(CurrencyDataRequest.INSTANCE, CurrenciesDTO.class, CurrenciesDTOMapper.INSTANCE);
+        currencyRepository.update(currencies);
+        LOG.info("Updated currency repository (" + currencies.size() + " items total)");
     }
 
-    private <D, T> T serve(AbstractDataRequest dataRequest, Class<D> dataClass, DTOMapper<D, T> dataMapper) throws ServiceException {
+    private <D, T> T fetch(DataRequest dataRequest, Class<D> dataClass, DTOMapper<D, T> dataMapper) throws ServiceException {
         try {
             URI resourceIdentifier = dataRequest.toURIWithHost(host);
             String responseContent = Request.get(resourceIdentifier)
@@ -95,11 +110,11 @@ public final class FrankfurterService {
             D dataObject = JSON_PARSER.readValue(responseContent, dataClass);
             T mappedData = dataMapper.map(dataObject);
 
-            LOGGER.info("Fetched: " + resourceIdentifier);
+            LOG.info("Fetched: " + resourceIdentifier);
             return mappedData;
 
         } catch (URISyntaxException | IOException | DTOMappingException e) {
-            LOGGER.error("Failed to fetch resource (cause: " + e.getCause() + ')');
+            LOG.error("Failed to fetch resource (cause: " + e.getCause() + ')');
             throw new ServiceException(e);
         }
     }
--- src/test/java/com/altoukhov/bratwurst/service/FrankfurterServiceTests.java
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: Copyright 2022 Maxim Altoukhov
-// SPDX-License-Identifier: MIT
-
-package com.altoukhov.bratwurst.service;
-
-import org.junit.jupiter.api.DisplayName;
-
-@DisplayName("FrankfurterService Tests")
-public final class FrankfurterServiceTests {} /* TODO */
