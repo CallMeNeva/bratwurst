ae11b1b5ef2aacf5cca91a31960a1f30538558aa
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-01 22:07:54 +0300

    PortNumberSpinner: remove "final" from class, change default initial value

--- src/main/java/com/altoukhov/bratwurst/gui/control/PortNumberSpinner.java
+++ src/main/java/com/altoukhov/bratwurst/gui/control/PortNumberSpinner.java
@@ -6,20 +6,23 @@ package com.altoukhov.bratwurst.gui.control;
 import com.altoukhov.bratwurst.gui.converter.PortNumberStringConverter;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.util.StringConverter;
 import org.apache.hc.core5.net.Ports;
 
-public final class PortNumberSpinner extends Spinner<Integer> {
+public class PortNumberSpinner extends Spinner<Integer> {
 
-    private static final PortNumberStringConverter STRING_CONVERTER = new PortNumberStringConverter();
+    private static final StringConverter<Integer> DEFAULT_STRING_CONVERTER = new PortNumberStringConverter();
 
     public PortNumberSpinner(int port) {
-        // No validation needed on "port" as spinner rounds externally set values within bounds (not sure though)
+        // NOTE: No validation needed on "port" as spinner rounds externally set values within bounds (not sure though)
+        // NOTE: Since the string converter is set *after* the initial value is, if it's the default port value it won't be converted until the next
+        // user input. This can be solved by simply setting the initial value to something else or at least setting it after the converter was.
         super(Ports.SCHEME_DEFAULT, Ports.MAX_VALUE, port, 1);
         SpinnerValueFactory<Integer> valueFactory = getValueFactory();
-        valueFactory.setConverter(STRING_CONVERTER);
+        valueFactory.setConverter(DEFAULT_STRING_CONVERTER);
     }
 
     public PortNumberSpinner() {
-        this(Ports.SCHEME_DEFAULT);
+        this(0);
     }
 }
