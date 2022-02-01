85bd5ba2bba2228ff0e19d5a32f26f7d99f9364d
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-01 03:40:21 +0300

    Add Arguments

---
+++ src/main/java/com/altoukhov/bratwurst/util/Arguments.java
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: Copyright 2022 Maxim Altoukhov
+// SPDX-License-Identifier: MIT
+
+package com.altoukhov.bratwurst.util;
+
+public final class Arguments {
+
+    private Arguments() {}
+
+    // This exists and is used instead of Objects::requireNonNull only to provide a generic exception message for null-checks. Instead of
+    // throwing an NPE by delegating to Objects::requireNonNull, an IllegalArgumentException is manually thrown simply because it's
+    // (supposedly) a better semantic fit. Either way, it's not a big deal.
+    public static <T> T checkNull(T argument, String argumentName) {
+        if (argument == null) {
+            throw new IllegalArgumentException("Provided argument \"" + argumentName + "\" is null");
+        }
+        return argument;
+    }
+}
