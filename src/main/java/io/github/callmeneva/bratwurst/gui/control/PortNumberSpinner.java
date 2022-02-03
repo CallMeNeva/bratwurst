c9c76a94d6eb20eacb61854b7f3666a15cb1283b
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-03 16:47:48 +0300

    PortNumberSpinner: set default value to non-magic constant

--- src/main/java/io/github/callmeneva/bratwurst/gui/control/PortNumberSpinner.java
+++ src/main/java/io/github/callmeneva/bratwurst/gui/control/PortNumberSpinner.java
@@ -16,13 +16,14 @@ public class PortNumberSpinner extends Spinner<Integer> {
     public PortNumberSpinner(int port) {
         // NOTE: No validation needed on "port" as spinner rounds externally set values within bounds (not sure though)
         // NOTE: Since the string converter is set *after* the initial value is, if it's the default port value it won't be converted until the next
-        // user input. This can be solved by simply setting the initial value to something else or at least setting it after the converter was.
+        // user input or programmatic change. This can be solved by simply setting the initial value to something else or at least setting the initial
+        // value *after* the converter.
         super(Ports.SCHEME_DEFAULT, Ports.MAX_VALUE, port, 1);
         SpinnerValueFactory<Integer> valueFactory = getValueFactory();
         valueFactory.setConverter(DEFAULT_STRING_CONVERTER);
     }
 
     public PortNumberSpinner() {
-        this(0);
+        this(Ports.MIN_VALUE);
     }
 }
