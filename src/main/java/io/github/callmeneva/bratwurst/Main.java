54d55671c0a65943c7f010cf30718192b2e84770
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-04 20:39:06 +0300

    Rename main class

--- src/main/java/io/github/callmeneva/bratwurst/Main.java
+++ src/main/java/io/github/callmeneva/bratwurst/Main.java
@@ -3,21 +3,15 @@
 
 package io.github.callmeneva.bratwurst;
 
-import io.github.callmeneva.bratwurst.gui.BratwurstFX;
+import io.github.callmeneva.bratwurst.gui.Bratwurst;
 import javafx.application.Application;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
-public final class Bratwurst {
+public final class Main {
 
     public static void main(String[] args) {
-        if (args.length > 0) {
-            // FIXME: Externalize UI strings
-            System.err.println("This program does not take any command line arguments!");
-            return;
-        }
-
         SLF4JBridgeHandler.removeHandlersForRootLogger();
         SLF4JBridgeHandler.install();
-        Application.launch(BratwurstFX.class, args);
+        Application.launch(Bratwurst.class, args);
     }
 }
