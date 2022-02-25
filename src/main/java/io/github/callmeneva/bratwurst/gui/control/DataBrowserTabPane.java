d991fb6638d2a3474ec018d2c53d22b6831b10ee
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-26 02:04:05 +0300

    Update DataBrowserTabPane

--- src/main/java/io/github/callmeneva/bratwurst/gui/control/DataBrowserTabPane.java
+++ src/main/java/io/github/callmeneva/bratwurst/gui/control/DataBrowserTabPane.java
@@ -3,13 +3,14 @@
 
 package io.github.callmeneva.bratwurst.gui.control;
 
-import io.github.callmeneva.bratwurst.gui.browser.DataBrowser;
+import io.github.callmeneva.bratwurst.gui.browser.AbstractDataBrowser;
 import io.github.callmeneva.bratwurst.gui.util.Resettable;
 import io.github.callmeneva.bratwurst.l10n.Localization;
 import io.github.callmeneva.bratwurst.service.DataFetchFailureException;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import org.apache.commons.lang3.Validate;
+
+import java.util.Objects;
 
 public class DataBrowserTabPane extends TabPane implements Resettable {
 
@@ -17,29 +18,30 @@ public class DataBrowserTabPane extends TabPane implements Resettable {
         setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
     }
 
-    public void addBrowser(DataBrowser<?, ?> dataBrowser, String l10nKey) {
-        Validate.notNull(dataBrowser);
+    public void addBrowser(AbstractDataBrowser<?, ?> dataBrowser, String l10nPropertyName) {
+        Objects.requireNonNull(dataBrowser, "Browser must not be null");
 
-        String title = Localization.getString(l10nKey);
-        Tab tab = new Tab(title, dataBrowser);
-
-        getTabs().add(tab);
+        String title = Localization.get(l10nPropertyName);
+        getTabs().add(new Tab(title, dataBrowser));
     }
 
-    public void submitSelected() throws DataFetchFailureException {
-        getSelectedBrowser().submitRequest();
-    }
-
-    private DataBrowser<?, ?> getSelectedBrowser() {
-        Tab selectedTab = getSelectionModel().getSelectedItem();
-        if (selectedTab.getContent() instanceof DataBrowser<?,?> browser) {
-            return browser;
-        }
-        throw new IllegalStateException();
+    public void fetchWithSelected() throws DataFetchFailureException {
+        getSelectedBrowser().fetch();
     }
 
     @Override
     public void reset() {
-        getSelectedBrowser().reset();
+        AbstractDataBrowser<?, ?> selectedBrowser = getSelectedBrowser();
+        if (selectedBrowser instanceof Resettable resettableBrowser) {
+            resettableBrowser.reset();
+        }
+    }
+
+    private AbstractDataBrowser<?, ?> getSelectedBrowser() {
+        Tab selectedTab = getSelectionModel().getSelectedItem();
+        if (selectedTab.getContent() instanceof AbstractDataBrowser<?, ?> browser) {
+            return browser;
+        }
+        throw new IllegalStateException("DataBrowserTabPane should only contain tabs with AbstractDataBrowser as their content");
     }
 }
