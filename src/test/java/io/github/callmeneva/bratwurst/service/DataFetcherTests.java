490e67ba1abd2cb6a384b2489e5c12bd97261cb1
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-26 00:41:09 +0300

    Add DataFetcher tests

---
+++ src/test/java/io/github/callmeneva/bratwurst/service/DataFetcherTests.java
@@ -0,0 +1,45 @@
+// SPDX-FileCopyrightText: Copyright 2022 Maxim Altoukhov
+// SPDX-License-Identifier: MIT
+
+package io.github.callmeneva.bratwurst.service;
+
+import io.github.callmeneva.bratwurst.service.request.CurrencyDataRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("DataFetcher")
+class DataFetcherTests {
+
+    @Test
+    @DisplayName("Throws when fetching with illegal host")
+    public void illegalHost() {
+        DataFetcher<?, CurrencyDataRequest, ?> fetcher = DataFetcher.ofCurrencies("This is probably not a legal host.");
+
+        Assertions.assertThrows(DataFetchFailureException.class, () -> fetcher.fetch(new CurrencyDataRequest()));
+    }
+
+    @Test
+    @DisplayName("Throws when fetching with empty host")
+    public void emptyHost() {
+        DataFetcher<?, CurrencyDataRequest, ?> fetcher = DataFetcher.ofCurrencies("");
+
+        Assertions.assertThrows(DataFetchFailureException.class, () -> fetcher.fetch(new CurrencyDataRequest()));
+    }
+
+    @Test
+    @DisplayName("Throws when fetching with blank host")
+    public void blankHost() {
+        DataFetcher<?, CurrencyDataRequest, ?> fetcher = DataFetcher.ofCurrencies("\t \n \r");
+
+        Assertions.assertThrows(DataFetchFailureException.class, () -> fetcher.fetch(new CurrencyDataRequest()));
+    }
+
+    @Test
+    @DisplayName("Throws when fetching with null host")
+    public void nullHost() {
+        DataFetcher<?, CurrencyDataRequest, ?> fetcher = DataFetcher.ofCurrencies(null);
+
+        Assertions.assertThrows(DataFetchFailureException.class, () -> fetcher.fetch(new CurrencyDataRequest()));
+    }
+}
