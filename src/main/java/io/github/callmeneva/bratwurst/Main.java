951e0158031823f693c75f37a0fd4af01f686dda
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-26 05:00:46 +0300

    Update entry point

--- src/main/java/io/github/callmeneva/bratwurst/Main.java
+++ src/main/java/io/github/callmeneva/bratwurst/Main.java
@@ -5,13 +5,10 @@ package io.github.callmeneva.bratwurst;
 
 import io.github.callmeneva.bratwurst.gui.Bratwurst;
 import javafx.application.Application;
-import org.slf4j.bridge.SLF4JBridgeHandler;
 
 public final class Main {
 
     public static void main(String[] args) {
-        SLF4JBridgeHandler.removeHandlersForRootLogger();
-        SLF4JBridgeHandler.install();
         Application.launch(Bratwurst.class, args);
     }
 }
